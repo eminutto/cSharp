7e8a959401c29a12e9470314a8085e5652b652be
Ezequiel Minutto <roockiesan@hotmail.com>
2019-10-29 06:24:54 -0300

    Add project files.

---
+++ Matrix/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SymmetricMatrix sm = new SymmetricMatrix();
+
+            sm.CheckIfSymmetricMatrix(new string[] { "5", "0", "4", "<>", "0", "5", "3", "<>", "2", "3", "2" });
+        }
+    }
+}
---
+++ Matrix/SymmetricMatrix.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class SymmetricMatrix
+    {
+        public SymmetricMatrix()
+        {
+
+        }
+
+        public string CheckIfSymmetricMatrix(string[] strArray)
+        {
+            int arrRow = 1;
+            int arrCol = 0;
+
+            foreach (var item in strArray)
+            {
+                if (item == "<>")
+                {
+
+                    arrRow++;
+
+                }
+                else if (arrRow == 1 && item != "<>")
+                {
+                    arrCol++;
+                }
+            }
+
+            if (arrRow != arrCol)
+            {
+                Console.WriteLine("Row: " + arrRow);
+                Console.WriteLine("Col: " + arrCol);
+                return "Not possible";
+            }
+            else
+            {
+                string[,] stringArray = new string[arrRow, arrCol];
+                string[,] transArray = new string[arrRow, arrCol];
+
+
+                arrRow = 0;
+                arrCol = 0;
+
+                for (int i = 0; i < strArray.Length; i++)
+                {
+                    if (strArray[i] == "<>")
+                    {
+                        arrRow++;
+                        arrCol = 0;
+                    }
+                    else
+                    {
+                        stringArray[arrRow, arrCol] = strArray[i];
+                        arrCol++;
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Row: " + arrRow);
+                Console.WriteLine("Col: " + arrCol);
+
+                for (int i = 0; i < stringArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < stringArray.GetLength(1); j++)
+                    {
+                        Console.Write(stringArray[i, j] + " ");
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                //Trans
+                for (int i = 0; i < arrCol; i++)
+                {
+                    for (int j = 0; j <= arrRow; j++)
+                    {
+                        transArray[j, i] = stringArray[i, j];
+                    }
+                }
+
+                //Display
+                for (int i = 0; i < transArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < transArray.GetLength(1); j++)
+                    {
+                        Console.Write(transArray[i, j] + " ");
+                    }
+                }
+
+                Console.WriteLine();
+                
+                //Compare
+                for (int i = 0; i < arrCol; i++)
+                {
+                    for (int j = 0; j <= arrRow; j++)
+                    {
+                        if (transArray[i, j] == stringArray[i, j])
+                        {
+                            Console.Write("{0}, {1} ", transArray[i, j], stringArray[i, j]);
+                            Console.WriteLine("Sym");
+                        }
+                        else
+                        {
+                            Console.Write("{0}, {1} ", transArray[i, j], stringArray[i, j]);
+                            return "not Symetric";
+                        }
+                    }
+                }
+                return "Symetric!!";
+            }
+        }
+
+    }
+}
